7ebcf4a79a3cf261e41614636daaa5ea627ca9f1
Stanley Lim <slim679975@gmail.com>
2020-02-03 18:46:08 -0500

    :sparkles: feat(about): Added about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-interface AboutProps {
+interface AboutProps {}
 
-}
-
-export const About: React.FC<AboutProps> = (props) => {
-    
+export const About: React.FC<AboutProps> = props => {
     return (
         <div>
             <div
@@ -24,9 +21,24 @@ export const About: React.FC<AboutProps> = (props) => {
                         zIndex: 1100
                     }}
                 >
-                    <div className="display-3 font-bold mt-5 text-white">Your privacy matters.</div>
+                    <div className="display-3 font-bold mt-5 text-white">About.</div>
                 </div>
             </div>
+            <section className="section--padded">
+                <div className="container">
+                    <p className="responsive-lead">
+                        This project was originally started back in December of 2011 and it has quickly evolved into a
+                        large project that it is today. As of 2017, it had already received over{' '}
+                        <b>1 million downloads</b> in over <b>165 countries</b>.
+                    </p>
+                    <p className="responsive-lead">
+                        I really hope to continue updating this project well into the future. Any feedback is welcome.
+                    </p>
+                    <picture>
+                        <img src="/images/5years_orig.png" />
+                    </picture>
+                </div>
+            </section>
         </div>
-    )
-}
+    );
+};
